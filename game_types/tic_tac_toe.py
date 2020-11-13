b802b29d0ac8c2d6e36dd36e00fdd7477927b32e
Tom Dalby <thomasjackdalby@gmail.com>
2020-11-13 19:01:46 +0000

    updated win logic

--- game_types/tic_tac_toe.py
+++ game_types/tic_tac_toe.py
@@ -14,19 +14,25 @@ def create():
 
 def turn(current_game_state, player_id, player_action):
     index = int(player_action)
-    if current_game_state[index] != ' ': return None
+    
+    # check if its a valid move
+    if current_game_state[index] != ' ':
+        return None, None, None
+    
+    # get next game state
     player_char = 'O' if player_id == 0 else 'X'
     next_game_state = current_game_state[:index] + player_char + current_game_state[index+1:]
-    next_player_id = (player_id + 1) % 2
 
-    win_player_icon = check_has_won(next_game_state)
-    win_player_id = None
-    if win_player_icon is not None:
-        win_player_id = 0 if win_player_icon == 'O' else 1
+    # work out the next player
+    if next((i for i, c in enumerate(game_data) if c == ' '), None) is None: next_player_id = None
+    else: next_player_id = (player_id + 1) % 2
+
+    # work out if a player has won
+    win_player_id = find_winning_player(next_game_state)
 
     return next_game_state, next_player_id, win_player_id
 
-def check_has_won(game_data):
+def find_winning_player(game_data):
     for option in options:
         player_icon = game_data[option[0]]
         if player_icon == ' ':
@@ -37,5 +43,5 @@ def check_has_won(game_data):
                 success = False 
                 break
         if success:
-            return player_icon
+            return 0 if player_icon == 'O' else 1
     return None
